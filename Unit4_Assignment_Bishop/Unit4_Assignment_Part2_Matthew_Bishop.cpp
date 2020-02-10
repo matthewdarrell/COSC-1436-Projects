d6555c70c8a32eff646e0dad7b9f2625f05ee9c4
matthewdarrell <39975598+matthewdarrell@users.noreply.github.com>
2020-02-10 11:14:53 -0600

    Update Unit4_Assignment_Part2_Matthew_Bishop.cpp

--- Unit4_Assignment_Bishop/Unit4_Assignment_Part2_Matthew_Bishop.cpp
+++ Unit4_Assignment_Bishop/Unit4_Assignment_Part2_Matthew_Bishop.cpp
@@ -1,19 +1,24 @@
 // Dr_T numbers with ordinals C++ Bishop
 // Date: 02/09/20 IDE: Visual Studio Code
 #include <iostream>
+#include "Input_Validators.h"
 #include <math.h>
 using namespace std;
 
-int main(int argc, char* argvx[]) 
+int main() 
 {
+  // declare variables
   int inputNumber = 0;
   string postfix = "";
-
-  inputNumber = atoi(argvx[1]);
-
+  
+  // input
+  cout << "Please enter a number: ";
+  inputNumber = getValidInt();
+  
+  // process
   for (int i = 1; i <= inputNumber; i++)
   {
-    if (i / 10 == 1)
+    if ((i % 100) / 10 == 1)
     {
       postfix = "th";
     }
@@ -33,9 +38,12 @@ int main(int argc, char* argvx[])
     {
       postfix = "th";
     }
+    
+    // output
     cout << i << postfix << " Hello" << endl;
   }
-
+  
+  // output
   cout << inputNumber;
 
   return 0;
